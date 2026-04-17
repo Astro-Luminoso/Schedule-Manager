78b60d820fa677252b218b9e1884cc932ee9a2af
hanbyeol.yang <beuri97@gmail.com>
2026-04-17 18:49:58 +0900

    feat: add two data transfer objects
    - Add SessionUser to store user info
    - Add PostLoginRequest dto to get request body contents

---
+++ src/main/java/dev/nbcsparta/assignment/schedulemanager/dto/SessionUser.java
@@ -0,0 +1,4 @@
+package dev.nbcsparta.assignment.schedulemanager.dto;
+
+public record SessionUser(Long id, String email) {
+}
---
+++ src/main/java/dev/nbcsparta/assignment/schedulemanager/dto/request/PostLoginRequest.java
@@ -0,0 +1,4 @@
+package dev.nbcsparta.assignment.schedulemanager.dto.request;
+
+public record PostLoginRequest(String email, String password) {
+}
