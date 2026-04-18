875dcd662176b118dcaf0bd9bc67fb2ecfbdaa7c
hanbyeol.yang <beuri97@gmail.com>
2026-04-19 01:52:09 +0900

    feat: implement logout request

--- src/main/java/dev/nbcsparta/assignment/schedulemanager/controller/LoginController.java
+++ src/main/java/dev/nbcsparta/assignment/schedulemanager/controller/LoginController.java
@@ -3,6 +3,7 @@ package dev.nbcsparta.assignment.schedulemanager.controller;
 import dev.nbcsparta.assignment.schedulemanager.dto.SessionUser;
 import dev.nbcsparta.assignment.schedulemanager.dto.request.PostLoginRequest;
 import dev.nbcsparta.assignment.schedulemanager.service.LoginService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -28,6 +29,16 @@ public class LoginController {
         SessionUser sessionUser = loginService.executeLogin(request);
 
         session.setAttribute("LOGIN_USER", sessionUser);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> attemptLogout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
