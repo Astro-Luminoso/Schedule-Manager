f0fccad3eabc45831f5f5005d31cabde31d66900
hanbyeol.yang <beuri97@gmail.com>
2026-04-19 01:03:20 +0900

    feat: update controller service and repository
    - implement logics based on test scripts

--- src/main/java/dev/nbcsparta/assignment/schedulemanager/service/LoginService.java
+++ src/main/java/dev/nbcsparta/assignment/schedulemanager/service/LoginService.java
@@ -1,13 +1,33 @@
 package dev.nbcsparta.assignment.schedulemanager.service;
 
+import dev.nbcsparta.assignment.schedulemanager.config.PasswordEncoder;
 import dev.nbcsparta.assignment.schedulemanager.dto.SessionUser;
 import dev.nbcsparta.assignment.schedulemanager.dto.request.PostLoginRequest;
+import dev.nbcsparta.assignment.schedulemanager.entity.Author;
+import dev.nbcsparta.assignment.schedulemanager.exception.AuthorNotFoundException;
+import dev.nbcsparta.assignment.schedulemanager.exception.PasswordNotMatchException;
+import dev.nbcsparta.assignment.schedulemanager.repository.AuthorRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
-    public SessionUser executeLogin(PostLoginRequest reqBody) {
 
-        return null;
+    private final AuthorRepository authorRepository;
+    private final PasswordEncoder encoder;
+
+    public LoginService(AuthorRepository authorRepository, PasswordEncoder encoder) {
+        this.authorRepository = authorRepository;
+        this.encoder = encoder;
+    }
+
+    public SessionUser executeLogin(PostLoginRequest reqBody) {
+        Author author = authorRepository.findByEmail(reqBody.email())
+                .orElseThrow(() -> new AuthorNotFoundException(HttpStatus.UNAUTHORIZED));
+        if(!author.isPasswordMatch(encoder, reqBody.password())) {
+            throw new PasswordNotMatchException(HttpStatus.UNAUTHORIZED);
+        }
+
+        return SessionUser.from(author);
     }
 }
