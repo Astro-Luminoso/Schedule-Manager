a7c662a39e115224c9d3b8016f58706346bcee41
hanbyeol.yang <beuri97@gmail.com>
2026-04-19 01:01:13 +0900

    feat: add new DTOs

--- src/main/java/dev/nbcsparta/assignment/schedulemanager/dto/request/PostLoginRequest.java
+++ src/main/java/dev/nbcsparta/assignment/schedulemanager/dto/request/PostLoginRequest.java
@@ -1,4 +1,15 @@
 package dev.nbcsparta.assignment.schedulemanager.dto.request;
 
-public record PostLoginRequest(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PostLoginRequest(
+        @Email
+        String email,
+
+        @Size(min = 8)
+        @NotBlank
+        String password) {
 }
