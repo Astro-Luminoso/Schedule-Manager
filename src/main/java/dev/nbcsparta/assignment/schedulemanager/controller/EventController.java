79036e9c6275cbf7e9a725931fd8f6739d6832ac
hanbyeol.yang <beuri97@gmail.com>
2026-04-20 18:31:04 +0900

    feat: implement controller

--- src/main/java/dev/nbcsparta/assignment/schedulemanager/controller/EventController.java
+++ src/main/java/dev/nbcsparta/assignment/schedulemanager/controller/EventController.java
@@ -1,8 +1,13 @@
 package dev.nbcsparta.assignment.schedulemanager.controller;
 
+import dev.nbcsparta.assignment.schedulemanager.dto.SessionUser;
+import dev.nbcsparta.assignment.schedulemanager.dto.request.PostEventRequest;
+import dev.nbcsparta.assignment.schedulemanager.dto.response.CommonEventResponse;
 import dev.nbcsparta.assignment.schedulemanager.service.EventService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
@@ -12,6 +17,18 @@ public class EventController {
 
     public EventController (EventService eventService) {
         this.eventService = eventService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CommonEventResponse> createNewEvent(
+            @Valid @RequestBody PostEventRequest reqBody,
+            @SessionAttribute(name = "LOGIN_USER", required = false) SessionUser sessionUser
+            ) {
+        if (sessionUser == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        CommonEventResponse response = eventService.createEvent(reqBody, sessionUser.id());
+        return ResponseEntity.status(201).body(response);
     }
 
 
--- src/main/java/dev/nbcsparta/assignment/schedulemanager/dto/request/PostEventRequest.java
+++ src/main/java/dev/nbcsparta/assignment/schedulemanager/dto/request/PostEventRequest.java
@@ -1,4 +1,15 @@
 package dev.nbcsparta.assignment.schedulemanager.dto.request;
 
-public record PostEventRequest(String title, String description) {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PostEventRequest(
+
+        @NotBlank
+        @Size(min = 4)
+        String title,
+
+        @NotBlank
+        String description) {
 }
