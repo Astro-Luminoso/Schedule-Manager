662a9fb9ecf5e0fade9aa8bbc9446152c3f47e7c
hanbyeol.yang <beuri97@gmail.com>
2026-04-21 02:14:05 +0900

    test: add unit test case for:
    - patch method
    - delete method

--- src/test/java/dev/nbcsparta/assignment/schedulemanager/unit/service/EventServiceTest.java
+++ src/test/java/dev/nbcsparta/assignment/schedulemanager/unit/service/EventServiceTest.java
@@ -1,9 +1,12 @@
 package dev.nbcsparta.assignment.schedulemanager.unit.service;
 
+import dev.nbcsparta.assignment.schedulemanager.dto.request.PatchEventRequest;
 import dev.nbcsparta.assignment.schedulemanager.dto.response.CommonEventResponse;
 import dev.nbcsparta.assignment.schedulemanager.dto.request.PostEventRequest;
 import dev.nbcsparta.assignment.schedulemanager.entity.Client;
 import dev.nbcsparta.assignment.schedulemanager.entity.Event;
+import dev.nbcsparta.assignment.schedulemanager.exception.ClientNotAuthorisedException;
+import dev.nbcsparta.assignment.schedulemanager.exception.EventNotFoundException;
 import dev.nbcsparta.assignment.schedulemanager.repository.EventRepository;
 import dev.nbcsparta.assignment.schedulemanager.service.ClientService;
 import dev.nbcsparta.assignment.schedulemanager.service.EventService;
@@ -13,10 +16,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
@@ -35,7 +39,7 @@ public class EventServiceTest {
     private EventService eventService;
 
     @Test
-    public void createNewEventAndSuccess() {
+    public void testCreateNewEventAndSuccess() {
         long dummyClientId = 1L;
         Client dummyClient = new Client("Test User", "john.doe@dummy.dev", "qwer1234");
         PostEventRequest request = new PostEventRequest("Test Event", "This is a test event.");
@@ -58,5 +62,94 @@ public class EventServiceTest {
         Assertions.assertEquals(request.description(), dummyResBody.description());
         Assertions.assertEquals(dummyClient.getUserName(), dummyResBody.authorName());
         Assertions.assertNotNull(dummyResBody.date());
+    }
+
+    @Test
+    public void testPatchEventAndSuccess() {
+        long dummyClientId = 1L;
+        Client dummyClient = new Client("Test User", "john.doe@dummy.dev", "qwer1234");
+        PatchEventRequest dummyReqBody = new PatchEventRequest("Test Event", "This is a test event.");
+        Event dummyEvent = new Event("dummyReqBody.title()", "dummyReqBody.description()", dummyClient);
+        ReflectionTestUtils.setField(dummyClient, "id", dummyClientId);
+        ReflectionTestUtils.setField(dummyEvent, "id", 1L);
+        ReflectionTestUtils.setField(dummyEvent, "updatedDate", LocalDateTime.now());
+
+        when(eventRepository.findById(1L)).thenReturn(Optional.of(dummyEvent));
+
+        CommonEventResponse dummyResBody = eventService.updateEvent(dummyEvent.getId(), dummyReqBody, dummyClientId);
+
+        Assertions.assertEquals(dummyEvent.getId(), dummyResBody.id());
+        Assertions.assertEquals(dummyReqBody.title(), dummyResBody.title());
+        Assertions.assertEquals(dummyReqBody.description(), dummyResBody.description());
+        Assertions.assertEquals(dummyClient.getUserName(), dummyResBody.authorName());
+    }
+
+    @Test
+    public void testPatchEventAndFailByNotFound() {
+        long dummyClientId = 1L;
+        PatchEventRequest dummyReqBody = new PatchEventRequest("Test Event", "This is a test event.");
+
+        when(eventRepository.findById(1L)).thenReturn(Optional.empty());
+
+        EventNotFoundException ex = Assertions.assertThrows(EventNotFoundException.class, () ->
+            eventService.updateEvent(1L, dummyReqBody, dummyClientId)
+        );
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, ex.getStatus());
+    }
+
+    @Test
+    public void testPatchEventAndFailByForbidden() {
+        long dummyClientId = 1L;
+        Client dummyClient = new Client("Test User", "dummy@guruguru.com", "qwer1234");
+        ReflectionTestUtils.setField(dummyClient, "id", 2L);
+        PatchEventRequest dummyReqBody = new PatchEventRequest("Test Event", "This is a test event.");
+        Event dummyEvent = new Event("dummyReqBody.title()", "dummyReqBody.description()", dummyClient);
+
+        when(eventRepository.findById(1L)).thenReturn(Optional.of(dummyEvent));
+
+        ClientNotAuthorisedException ex = Assertions.assertThrows(ClientNotAuthorisedException.class, () ->
+            eventService.updateEvent(1L, dummyReqBody, dummyClientId)
+        );
+        Assertions.assertEquals(HttpStatus.FORBIDDEN, ex.getStatus());
+    }
+
+    @Test
+    public void testDeleteEventAndSuccess() {
+        Client dummyClient = new Client("Test User", "john.doe@dummy.dev", "qwer1234");
+        Event dummyEvent = new Event("dummyReqBody.title()", "dummyReqBody.description()", dummyClient);
+        ReflectionTestUtils.setField(dummyClient, "id", 1L);
+        ReflectionTestUtils.setField(dummyEvent, "id", 1L);
+
+        when(eventRepository.findById(1L)).thenReturn(Optional.of(dummyEvent));
+
+        eventService.deleteEvent(dummyEvent.getId(), dummyClient.getId());
+        verify(eventRepository).delete(dummyEvent);
+    }
+
+    @Test
+    public void testDeleteEventFailedByNotFound() {
+        when(eventRepository.findById(1L)).thenReturn(Optional.empty());
+
+        EventNotFoundException ex = Assertions.assertThrows(EventNotFoundException.class, () ->
+            eventService.deleteEvent(1L, 1L)
+        );
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, ex.getStatus());
+    }
+
+    @Test
+    public void testDeleteEventFailedByForBidden() {
+        Client dummyClient = new Client("Test User", "dummy@guruguru.com", "qwer1234");
+        ReflectionTestUtils.setField(dummyClient, "id", 2L);
+        Event dummyEvent = new Event("dummyReqBody.title()", "dummyReqBody.description()", dummyClient);
+
+        when(eventRepository.findById(1L)).thenReturn(Optional.of(dummyEvent));
+
+        ClientNotAuthorisedException ex = Assertions.assertThrows(ClientNotAuthorisedException.class, () ->
+            eventService.deleteEvent(1L, 1L)
+        );
+        Assertions.assertEquals(HttpStatus.FORBIDDEN, ex.getStatus());
+
+
     }
 }
