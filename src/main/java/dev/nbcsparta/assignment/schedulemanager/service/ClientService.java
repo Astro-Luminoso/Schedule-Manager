02c1a5f27c6916df36666cd013fae17043640298
hanbyeol.yang <beuri97@gmail.com>
2026-04-21 23:21:19 +0900

    feat: update ClientService and add ClientController

--- src/main/java/dev/nbcsparta/assignment/schedulemanager/service/ClientService.java
+++ src/main/java/dev/nbcsparta/assignment/schedulemanager/service/ClientService.java
@@ -1,12 +1,15 @@
 package dev.nbcsparta.assignment.schedulemanager.service;
 
+import dev.nbcsparta.assignment.schedulemanager.dto.response.ClientsInList;
 import dev.nbcsparta.assignment.schedulemanager.entity.Client;
 import dev.nbcsparta.assignment.schedulemanager.exception.AuthorNotFoundException;
 import dev.nbcsparta.assignment.schedulemanager.repository.ClientRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ClientService {
 
     private final ClientRepository clientRepository;
@@ -15,8 +18,14 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
+    @Transactional(readOnly = true)
     public Client retrieveClientById(long clientId) {
         return clientRepository.findById(clientId)
                 .orElseThrow(() -> new AuthorNotFoundException(HttpStatus.NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientsInList retrieveAllClients() {
+        return new ClientsInList(clientRepository.findAll());
     }
 }
