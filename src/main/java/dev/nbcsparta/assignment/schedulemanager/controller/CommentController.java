e9b6cb8edf08a4bc3cdd01a957083a9084e8f4f5
hanbyeol.yang <beuri97@gmail.com>
2026-04-23 05:17:46 +0900

    feat: implement Comment controller

---
+++ src/main/java/dev/nbcsparta/assignment/schedulemanager/controller/CommentController.java
@@ -0,0 +1,38 @@
+package dev.nbcsparta.assignment.schedulemanager.controller;
+
+import dev.nbcsparta.assignment.schedulemanager.dto.SessionUser;
+import dev.nbcsparta.assignment.schedulemanager.dto.request.PostNewComment;
+import dev.nbcsparta.assignment.schedulemanager.dto.response.CommentDetail;
+import dev.nbcsparta.assignment.schedulemanager.exception.ClientNotAuthorisedException;
+import dev.nbcsparta.assignment.schedulemanager.service.CommentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+@RequestMapping("/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentDetail> createNewComment(
+            @Valid @RequestBody PostNewComment reqBody,
+            @SessionAttribute(name = "LOGIN_USER", required = false)SessionUser sessionUser
+    ) {
+        if (sessionUser == null) {
+            throw new ClientNotAuthorisedException(HttpStatus.UNAUTHORIZED);
+        }
+        CommentDetail resBody = commentService.createComment(reqBody, sessionUser.id());
+        return ResponseEntity.status(HttpStatus.CREATED).body(resBody);
+    }
+}
